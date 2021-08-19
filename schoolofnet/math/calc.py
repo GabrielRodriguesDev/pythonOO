b8217314e066e41fbba9698f99dc5f999d2cc509
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-08-19 01:01:16 -0300

    Primeiro commit no primeiro projeto de POO com python

---
+++ schoolofnet/math/calc.py
@@ -0,0 +1,5 @@
+def calculate(x, y):
+    print(x+y)
+    
+def calculateSub(x, y):
+    print(x-y)
---
+++ schoolofnet/string/str.py
@@ -0,0 +1,5 @@
+def sumString(str1, str2, str3):
+    print(str1 + ' ' + str2 + ' ' + str3)
+
+#def sumString():
+#    print('to aqui')22
