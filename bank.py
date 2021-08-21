adeed67154dc6554859f7181e88560fc5dd67a6f
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-08-21 00:27:15 -0300

    Aula de modificadores de acesso e herança

--- bank.py
+++ bank.py
@@ -1,16 +1,28 @@
-class Account:
+class Bank1Account(object):
     
     def __init__(self, number):
         self.number = number
-        self.total = 0
+        self.__total = 0
         
-# Conceito de encapsulamento de código, deixamos o atributo "total" para sofrer alterações ou consultas através
-# de métodos oque nós trás uma maior confiança sobre o processo. Não devem acessar ou alterar o atributo direto.
     def deposit(self, value):
-        self.total += value
+        self.__total += value
         
-    def withdraw(self, value):
-        self.total -= value
+    def withDraw(self, value):
+        self.__total -= value
+        self._total -= 1
         
     def getTotal(self):
-        return self.total
+        return self.__total
+
+class Bank2Account(Bank1Account):
+    
+    def __init__(self, number, cvv):
+        super().__init__(number) 
+        self.cvv = cvv 
+
+
+    def withDraw(self, value):
+        self._Bank1Account__total -= value 
+        self._Bank1Account__total -= 2
+
+    
